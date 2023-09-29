6090f69344c2410199969e0e490acf5b9babd0b0
RRedBaron <nedelchevevg@gmail.com>
2023-09-29 22:07:30 +0300

    Formatted Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,33 @@
-import React from 'react'
-import Plus from './Icons/Plus.tsx'
-import Search from './Icons/Search.tsx'
-import Home from './Icons/Home.tsx'
-import styles from './IHeader.module.scss'
-import HeaderButton from './HeaderButton/HeaderButton.tsx'
-import {Link} from 'react-router-dom'
+import React from "react";
+import Plus from "./Icons/Plus.tsx";
+import Search from "./Icons/Search.tsx";
+import Home from "./Icons/Home.tsx";
+import styles from "./IHeader.module.scss";
+import HeaderButton from "./HeaderButton/HeaderButton.tsx";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
-
-    return (
+  return (
     <header className={styles.header}>
-        <Link className={styles.logo} to={'/'}>
-            <img src="/title.png" alt="Mengram" />
-        </Link>
-        <Link to={'/'}>
-            <HeaderButton
-                text={'home'}
-                func={() => {}}
-            >
-                <Home fill={'white'} style={{width: '30px', height: '30px', marginRight: '-6px'}}/>
-            </HeaderButton>
-        </Link>
-        <HeaderButton
-            text={'search'}
-            func={() => {}}
-        >
-            <Search fill={'white'}/>
+      <Link className={styles.logo} to={"/"}>
+        <img src="/title.png" alt="Mengram" />
+      </Link>
+      <Link to={"/"}>
+        <HeaderButton text={"home"} func={() => {}}>
+          <Home
+            fill={"white"}
+            style={{ width: "30px", height: "30px", marginRight: "-6px" }}
+          />
         </HeaderButton>
-        <HeaderButton
-            text={'create'}
-            func={() => {}}
-        >
-            <Plus fill={'white'}/>
-        </HeaderButton>
+      </Link>
+      <HeaderButton text={"search"} func={() => {}}>
+        <Search fill={"white"} />
+      </HeaderButton>
+      <HeaderButton text={"create"} func={() => {}}>
+        <Plus fill={"white"} />
+      </HeaderButton>
     </header>
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
