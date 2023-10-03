7b7ba68f6b4d68daea84897ffc90499e6331da4a
RRedBaron <nedelchevevg@gmail.com>
2023-10-03 17:21:44 +0300

    Fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+import { NotFoundPage } from "./routes/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
